45f48f88bf092ce0a813c59b17fb9045fda0a8ff
aleksa-stojsic <stojsic.aleksa@gmail.com>
2022-06-07 17:34:24 +0200

    Fix: dark theme flickering on page load #4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import '../styles/globals.css';
 import Head from 'next/head';
-import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,7 +7,6 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-      <Script src='/theme.js' strategy='beforeInteractive' />
       <Component {...pageProps} />
     </>
   );
--- public/theme.js
+++ public/theme.js
@@ -1,7 +1,6 @@
 (function initTheme() {
   var theme = localStorage.getItem('theme') || 'light';
   if (theme === 'dark') {
-    localStorage.setItem('theme', 'dark');
     document.querySelector('html').setAttribute('data-theme', 'dark');
   }
 })();
