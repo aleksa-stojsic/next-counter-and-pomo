0106446bf041555dd4a1cc0723a13c7f0289c234
aleksa-stojsic <stojsic.aleksa@gmail.com>
2022-06-07 17:04:40 +0200

    Fix: dark theme flickering on page load #3

--- public/theme.js
+++ public/theme.js
@@ -2,6 +2,6 @@
   var theme = localStorage.getItem('theme') || 'light';
   if (theme === 'dark') {
     localStorage.setItem('theme', 'dark');
-    document.documentElement.setAttribute('data-theme', 'dark');
+    document.querySelector('html').setAttribute('data-theme', 'dark');
   }
 })();
